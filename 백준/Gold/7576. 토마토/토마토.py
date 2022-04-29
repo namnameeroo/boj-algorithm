ba3d0ba2991f91011e6661c9128e6e20f5fb4ad5
namnameeroo <69442847+namnameeroo@users.noreply.github.com>
2022-04-29 10:27:40 +0900

    [Gold V] Title: 토마토, Time: 2268 ms, Memory: 226260 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,26 +1,22 @@
 import sys
 from collections import deque
+sys.setrecursionlimit(10**4)
 input = sys.stdin.readline
 
 m,n = map(int, input().split())
 g = [list(map(int, input().split())) for _ in range(n) ]
 queue = deque([])
 
-# 익은 애들 x,y 좌표 구해서 queue에 넣기
 for i in range(n):
     for j in range(m):
         if g[i][j] == 1:
             queue.append([i,j])
-# 2<= m 행의 수, 높이, y < 1001
-# 2<= n 열의 수, 너비, x < 1001
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 def BFS():
-    # 익은 애들 꺼내
     while queue:
         x, y = queue.popleft()
-
         for i in range(4):
             a = x+dx[i]
             b = y+dy[i]
