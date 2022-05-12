b2d17165ec2664294bd5d1bcce5b327cb56d80ba
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2022-05-12 23:56:31 +0900

    [Silver IV] Title: 스택, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -2,34 +2,25 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 lst = []
-
 for i in range(n):
-    res = 0
     l =  list(input().split())
-
     if len(l)==2:   # push
-
         if l[0] == 'push':
             lst.append(l[1])
     else:
-
         order = l[0]
-        # print(order)
         if order == 'pop':
             if len(lst)==0:
                 print(-1)
                 continue
-            res = lst.pop()
+            print(lst.pop())
+
         elif order == 'size':
             print(len(lst))
-            # continue
         elif order == 'empty':
             print(1 if len(lst)==0 else 0)
         elif order == 'top':
             if len(lst)==0:
                 print(-1)
                 continue
-            res = lst[-1]
-
-    if res !=0:
-        print(int(res))
+            print(lst[-1])
