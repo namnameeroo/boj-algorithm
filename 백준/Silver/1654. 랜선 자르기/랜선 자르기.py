a9e6dfebe050def838ce73a6eabf75d0820a04f7
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2022-05-16 13:36:16 +0900

    [Silver III] Title: 랜선 자르기, Time: 112 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,30 +1,37 @@
 import sys
 input = sys.stdin.readline
+k, n = map(int, input().split())
 
-l, m = map(int, input().split())
-lst = []
-lar = 0
-for i in range(l):
-    temp = int(input())
-    lst.append(temp)
-    lar = max(lar, temp)
-
-MaxLen = lar
-MinLen = 1
+# lan = [int(input()) for _ in range(k)]
+long = -1
+lan = []
+for _ in range(k):
+    i = int(input())
+    long=max(long,i)
+    lan.append(i)
 
 
-def Count(len):
+# n개의 랜선 만들어야 함
+# 이미 k개 있음
+# 같은 길이로 만들자
+# 최대 길이는?
+
+def getCnt(mid):
+    tot = 0
     cnt = 0
-    for p in lst:
-        cnt += (p//len)
-    return cnt
+    for i in range(k):
+        cnt = lan[i]//mid
+        tot += cnt
+    return tot
 
-while (MinLen <= MaxLen) :
-    mid = (MaxLen + MinLen) //2
-    if Count(mid) >= m:
-        MinLen = mid+1
-        res = mid
-    else :
-        MaxLen = mid-1
-
-print(res)
+low = 1
+high = long
+ans = 0
+while low <= high:
+    mid = (low + high)//2
+    if getCnt(mid) >= n:
+        ans = mid
+        low = mid +1
+    else:
+        high = mid-1
+print(ans)
