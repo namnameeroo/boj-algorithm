0b94f3321b0a10b27bb7a5caf8138211dd97c7e5
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2022-05-17 16:23:41 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 2444 ms, Memory: 54356 KB -BaekjoonHub

---
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+def war(yp,yh):
+    mid = yh
+    for l in g:
+        a = l[1]
+        h = l[2]
+        if l[0] == 1: # 몬스터 만남
+            mp = a
+            mh = h
+            round = (mh // yp)
+            if mh % yp == 0:
+                yh -= mp * (round-1)
+            else:
+                yh -= mp * (round)
+            if yh < 1:
+                return False
+        else: # 포션방
+            yp += a
+            yh = min(yh+h ,mid)
+    return True
+
+
+n, yp = map(int, input().split())
+g = [ list(map(int, input().split())) for _ in range(n)]
+
+low = 1
+high = 10**17
+# high = sum(max([gg[1]*gg[2]] for gg in g))
+
+answer = 0
+while low <= high:
+    mid = (low+high)//2
+    if war(yp, mid) == False:
+        low = mid + 1
+
+    else:
+        high = mid - 1
+        answer = mid
+
+print(answer)
