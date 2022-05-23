7deaff7574832b00315e7425cc885116727d6825
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2022-05-23 22:17:49 +0900

    [Silver I] Title: 회의실 배정, Time: 304 ms, Memory: 57760 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,16 +1,14 @@
 import sys
 input = sys.stdin.readline
 n = int(input().rstrip())
-meeting = [list(map(int, input().split())) for _ in range(n)]
-meeting.sort(key=lambda x: (x[1],x[0]))
+meetings = [list(map(int, input().split())) for _ in range(n)]
+meetings.sort(key= lambda x: (x[1], x[0]))
 
-ss, ee = meeting[0][0], meeting[0][1]
-cnt = 1
-
-for i in meeting[1:]:
-    s,e = i[0], i[1]
-    if ee <= s: # 다음회의보다 일찍 끝
+time = -1
+cnt = 0
+for m in meetings:
+    if m[0] >= time:
+        time = m[1]
         cnt += 1
-        ee = e
 
 print(cnt)
