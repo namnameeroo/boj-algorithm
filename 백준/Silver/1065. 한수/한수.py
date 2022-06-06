4e896e90cbf2310bdd853280e5d1d4f177997d58
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2022-06-06 22:15:30 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,19 +1,10 @@
 x = int(input())
 cnt = 0
-
-for n in range(1,int(x)+1):
-    if n//10 <=9:
-        cnt += 1
-    lst = [int(nn) for nn in str(n)]
-
-    if len(lst)>2:
-        d = lst[0] - lst[1]
-        flag = True
-        for idx in range(len(lst)-1):
-            if d != lst[idx] - lst[idx+1]:
-                flag = False
-                break
-        if flag:
+if x < 100:
+    print(x)
+else:
+    cnt = 99
+    for s in range(111, x+1):
+        if int(str(s)[2]) - int(str(s)[1]) == int(str(s)[1]) - int(str(s)[0]):
             cnt += 1
-
-print(cnt)
+    print(cnt)
