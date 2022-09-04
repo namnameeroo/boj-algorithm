f16e12ea9b8f107a912ac71732aa5a2aaaa75224
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2022-09-04 23:55:45 +0900

    [Gold IV] Title: 공유기 설치, Time: 216 ms, Memory: 123888 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,32 +1,25 @@
-# 집 n개
-# 공유기 c개
-# 한 집에 하나, 가장 멀리씩 설치 max d
-
 import sys
 input = sys.stdin.readline
-n,c = map(int, input().split())
-house = sorted([int(input()) for _ in range(n)])
+n, c = map(int, input().split())
+houses = []
+for _ in range(n):
+    houses.append(int(input()))
+houses.sort()
 
-def getCnt(mid):
+left = 1
+right = max(houses) # 가장 먼 집을 기준으로
+while left <= right:
+    mid = (left+right)//2
     cnt = 1
-    idx = house[0]
+    cur = houses[0]
+    for i in range(1, len(houses)):
+        if houses[i] >= cur + mid:
+            cnt += 1
+            cur = houses[i]
 
-    for i in house[1:]:
-        if i >= idx + mid:
-            idx = i
-            cnt +=1
-
-    return cnt
-
-low = 1
-high = house[-1] - house[0]
-while low<=high:
-    mid = (low + high)//2
-    # 공유기 거리
-    if getCnt(mid) >= c:
-        answer = mid
-        low = mid+1
+    if cnt >= c:
+        left = mid + 1
     else:
-        high = mid-1
+        right = mid - 1
 
-print(answer)
+print(right)
