42985f651f735a5f81fd46b1177ac1d9bb2db9b3
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2022-09-08 15:13:44 +0900

    [level 2] Title: 오픈채팅방, Time: 143.20 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,24 +1,23 @@
-from collections import deque
-
-
-msg = {"out": "님이 나갔습니다.", "in": "님이 들어왔습니다."}
-
-
 def solution(record):
-    users = {}
-    inout_list = deque()
 
-    for row in record:
-        temp = list(row.split())
-        cmd, user, nick = temp[0], temp[1], temp[2] if len(temp) > 2 else ""
+    msg = {'Leave':'님이 나갔습니다.', 'Enter':'님이 들어왔습니다.'}
+    record = [line.split() for line in record]
+    users_dic = {}
+    result = []
+    for line in record:
+        cmd = line[0]
+        user = line[1]
+        if len(line) == 3:
+            users_dic[user] = line[2]
+        if line[0] in msg.keys():
+            result.append([cmd, user])
+    
+    txt_list = []
+    
+    for do in result:
+        cmd = do[0]
+        userid = do[1]
+        txt_list.append(''.join([users_dic[userid],msg[cmd]]))
+        
 
-        if cmd == "Change":
-            users[user] = nick
-        elif cmd == "Enter":
-            users[user] = nick
-            inout_list.append([user, "in"])
-        elif cmd == "Leave":
-            inout_list.append([user, "out"])
-
-    return ["".join([users[id], msg[mid]]) for id, mid in inout_list]
-
+    return txt_list
