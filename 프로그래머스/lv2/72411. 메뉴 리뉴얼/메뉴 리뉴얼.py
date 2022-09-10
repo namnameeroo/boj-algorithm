55660a96fdae4c5f30da7236cb53fd40e5c8c813
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2022-09-10 23:57:01 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.83 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,16 +1,15 @@
 from itertools import combinations
 
-
 def solution(orders, course):
     comb_dic = {long: {} for long in course}
     selected = set()
-
+    
     for long in course:
         best_num = 2
         for guest in orders:
             comb = list(map(sorted, combinations(guest, long)))
-            comb = ["".join(arr) for arr in comb if len(arr) > 0]
-
+            comb = ["".join(arr) for arr in comb if len(arr)>0]
+            
             for e in comb:
                 if not e:
                     continue
@@ -20,10 +19,9 @@ def solution(orders, course):
                     best_num = max(best_num, comb_dic[long][key])
                 else:
                     comb_dic[long][key] = 1
-
-        for k, v in comb_dic[long].items():
-            if v >= best_num:
+        for k,v in comb_dic[long].items():
+            if v>= best_num:
                 selected.add(k)
-
     selected = sorted(list(selected))
     return selected
+    
