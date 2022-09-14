fd906edffe55ed824d518a03e80a950bec34821f
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2022-09-14 13:54:21 +0900

    [level 2] Title: 순위 검색, Time: 1286.75 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.py
@@ -1,52 +1,90 @@
 from itertools import combinations
+# from bisect import bisectleft
+a = ['cpp', 'java', 'python']
+b = ['backend', 'frontend']
+c = ['junior', 'senior']
+d = ['chicken', 'pizza']
 
+# 리스트 탐색 => binary
+def getCnt(users, scoreCut):
+    userLong = len(users)
+    if userLong == 0:
+        return 0
+    if userLong == 1 :
+        if int(users[0]) < scoreCut:
+            return 0
+        else:
+            return 1
 
+    s = 0
+    e = userLong-1
     
+    while s <= e:
+        mid = (s+e)//2
+        userScore = users[mid]
+        if userScore < scoreCut:
+            s = mid + 1
+        if userScore >= scoreCut:
+            e = mid - 1
+
+    return userLong-s
+
+
 
 def solution(info, query):
-    answer = []
-    dic = {}
-    for person in info:
-        content_key = person.split()
-        score = int(content_key[-1])
-        for i in range(5):
-            comb =list(combinations(content_key[:4], i))
-            for c in comb:
-                c = ''.join(c)
-                if dic.get(c):
-                    dic[c].append(score)
-                else:
-                    dic[c] = [score]
+    utils = {name: id for id, name in enumerate([0]+a+b+c+d)}
+    scoreTable = {}
 
-    # dic = {k:sorted(v) for k, v in dic.items()}
-    for k in dic.values():
-        k.sort()
+    for user in info:
+        row_user = user.split()
+        # (a,b,c,d)
+        # (_,b,c,d)
+        # (a,_,c,d)
+        # ...
+        # (a,b,c,_)
+        # (_,_,_,_)
         
+        content, userScore = row_user[:4], int(row_user[-1])
+        for long in range(5):
+            keylist = list(combinations(content, long))
+            # scoreTable = {''.join([str(utils[name]) for name in k]):[userScore] for k in keylist} # {코드 : 점수, 코드 : 점수}
+            for i in keylist:
+                keyCode = ''.join([str(utils[name]) for name in i])
+                if scoreTable.get(keyCode):
+                    scoreTable[keyCode].append(userScore)
+                else:
+                    scoreTable[keyCode] = [userScore]
+                    
+
+    for k in scoreTable.keys():
+        scoreTable[k].sort()
+    
+
+
+    # query
+    ans = []
     for q in query:
-        q = [x for x in q.split() if (x!='and' and x!='-')]
+        row_query = q.replace('and','').split()
+        # conditions, standScore = row_user[:-2], row_user[-1]
+        conditions = [e for e in q.split() if e != 'and' and e !='-']
+        qCode = ''.join([str(utils[name]) for name in conditions[:-1]])
         
-        q_key = ''.join(q[:-1])
-        q_score = int(q[-1])
+        # qCode = ''.join(str(utils[name]) for name in )
+        standScore = int(conditions[-1])
         cnt = 0
+        if scoreTable.get(qCode):
+            cnt = getCnt(scoreTable[qCode], standScore)
+        ans.append(cnt)
+    return ans
+            
+
+            
+            
         
-        if dic.get(q_key):
-            v_sorted = dic[q_key]
-            if len(dic[q_key]) == 1 :
-                cnt = 1 if dic[q_key][0] >= q_score else 0
-            elif v_sorted[-1] < q_score:
-                cnt = 0
-            else:
-                s = 0
-                e = len(v_sorted)-1
-                
-                while s<=e:
-                    mid = (s+e)//2
-                    if v_sorted[mid] >= q_score:  # go to smaller
-                        e = mid-1
-                    if v_sorted[mid] < q_score:  # go to bigger
-                        s = mid+1
-                cnt = len(v_sorted)-s
-                
-        answer.append(cnt)
         
-    return answer
+
+        
+        
+        
+        
+
