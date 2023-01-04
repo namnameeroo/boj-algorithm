b10451dfbb83a44d7b6722c1442a2ca6f9d64e43
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-01-04 23:40:45 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.99 ms, Memory: 10.5 MB - Simple ver.

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,20 +1,15 @@
 import re
 def solution(files):
     answer = []
-    heads = []
     for idx, each_file in enumerate(files):
-        # exacted = ''.join([x for x in each_file if x.isdecimal()])
-        
         x = re.findall(r'([a-zA-Z\ \.\-]+)([0-9]+)(.*)', each_file)
         HEAD, BODY, TAIL = map(str, x[0])
-        answer.append([HEAD.upper(), int(BODY), idx])        
+        lst_for_order = [HEAD.upper(), int(BODY), idx]
+        answer.append(lst_for_order)
         
     answer.sort()
-    final = []
-    for i in answer:
-        idx = i[2]
-        final.append(files[idx])
+    final = [files[lst_for_order[2]] for lst_for_order in answer]
+    
     return final
-    # return [x[3] for x in answer]
     
     
