e2a0c68515a9065bb84b202ec5c05fa5db10bd9d
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-01-06 22:26:29 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -1,19 +1,15 @@
 from itertools import combinations
 def solution(dots):
-    answer = 0
 
     coms = list(combinations(dots, 2))
     for i in range(3):
         a,b = coms[i]
         c,d = coms[5-i]
-        print(a,b,c,d)
         
         angle_1 = (a[1]-b[1])/(a[0]-b[0])
         angle_2 = (c[1]-d[1])/(c[0]-d[0])
-        print(angle_1, angle_2)
+        
         if angle_1 == angle_2:
             return 1
-        
-        
     
     return 0
