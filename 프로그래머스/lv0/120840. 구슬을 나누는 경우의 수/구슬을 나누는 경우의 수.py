d8e7302226a31c56dcaac39cbb3c45dca1d7894d
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-01-09 15:55:25 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,14 +1,16 @@
+import math
+def solution(balls, share):
+    return math.comb(balls, share)
 
-def solution(balls, share): # 각각은 구슬 개수
-    # 5개 중에 2개의 고르기
-    result = 1
-    if balls==share:
-        return 1
+# def solution(balls, share): # 각각은 구슬 개수
+#     # 5개 중에 2개의 고르기
+#     result = 1
+#     if balls==share:
+#         return 1
     
-    share = min(share, balls-share)
-    for n in range(balls-share,balls):
-        result*=(n+1)
-    for m in range(share):
-        result /= (m+1)
-    
-    return int(result)
+#     share = min(share, balls-share)
+#     for dx in range(share):
+#         result *= balls-dx
+#         result /= (dx+1)
+        
+#     return int(result)
