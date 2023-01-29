75f93814cf14275e47ae5d957a95f93fb9f917df
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-01-29 20:32:03 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,12 +1,3 @@
 def solution(array, n):
-    answer = 0
-    smallers = -1
-    biggers = 10**3
-    
-    for num in array:
-        if num <= n:
-            smallers = max(smallers, num)
-        else:
-            biggers = min(biggers, num)
-    # print(smallers, biggers)
-    return smallers if n - smallers <= biggers - n else biggers
+    array.sort(key = lambda x: (abs(x-n), x)) 
+    return array[0]
