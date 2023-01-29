92635ff159147e2053eb81be0b5008afa9135ac1
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-01-29 20:46:55 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,8 +1,13 @@
 def solution(cipher, code):
-    answer = ''
+    return cipher[code-1::code]
     
-    for idx in range(len(cipher)):
-        if (idx + 1) % code == 0:
-            answer += cipher[idx]
     
-    return answer
+# 풀이 1    
+    # for idx in range(len(cipher)):
+    #     if (idx + 1) % code == 0:
+    #         answer += cipher[idx]
+    
+    
+# 풀이 2
+
+    # answer = ''.join([cipher[x] for x in range(len(cipher)) if (x+1)%code == 0])    
