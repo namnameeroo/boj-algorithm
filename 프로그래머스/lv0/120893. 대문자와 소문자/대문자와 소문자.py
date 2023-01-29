02709f0ef810482d0113859cf9cda4733fd5c8c0
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-01-29 20:49:08 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.14 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = ''
+    for letter in my_string:
+        if letter.isupper():
+            answer+= letter.lower()
+        else:
+            answer += letter.upper()
+    return answer
