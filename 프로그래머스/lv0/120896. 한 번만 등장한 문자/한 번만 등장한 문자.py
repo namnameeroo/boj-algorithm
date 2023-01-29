cd9ec5fe442cdc783eab4545a20eddac706bda79
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-01-29 21:44:40 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,4 +1,22 @@
-from collections import Counter
+
+
 def solution(s):
-    answer = ''.join(sorted([k for k, cnt in Counter(s).items() if cnt == 1]))
-    return answer
+    
+    return ''.join(sorted([ x for x in set(s) if s.count(x)==1]))
+
+# 풀이 1
+# from collections import Counter
+# answer = ''.join(sorted([k for k, cnt in Counter(s).items() if cnt == 1]))
+    
+
+# 풀이 2
+# def solution(s):
+#     hist = set()
+#     onlys = set()
+#     for each in s:
+#         if each not in hist:
+#             onlys.add(each)
+#             hist.add(each)
+#         else:
+#             onlys.discard(each)
+#     return ''.join(sorted(onlys))
