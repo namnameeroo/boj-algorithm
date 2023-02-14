b7cd368e804d81ca7b636d2a377afc3f3c201efb
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-02-14 17:17:30 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
@@ -0,0 +1,21 @@
+def solution(score):
+
+    avg_list = [[(x[0]+x[1])/2, idx] for idx, x in enumerate(score)]
+    # avg_list = [평균, 자리번호]
+    
+    # ranked[자리번호] = 랭크번호
+    ranked = [-1]*len(score)
+    rank = 0
+    same_cnt = 0
+    pre = -1
+    for avg, id in sorted(avg_list, reverse=True):
+        num = avg_list[id][0]
+        if pre != num:
+            rank = rank + 1 + same_cnt
+            same_cnt = 0
+            pre = num
+        else:
+            same_cnt += 1
+        ranked[id] = rank
+        
+    return ranked
