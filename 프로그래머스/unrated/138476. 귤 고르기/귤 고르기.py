078676ebd74ade5657b486306436be8bb7a9378b
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-02-23 17:10:37 +0900

    [unrated] Title: 귤 고르기, Time: 19.81 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,16 +1,9 @@
 from collections import Counter
 def solution(k, tangerine):
-    # c_list = list([x,y] for x, y in Counter(tangerine).items())
-
-    c_list = list(Counter(tangerine).items())
-    c_list.sort(key = lambda x: (x[1],x[0]),reverse=True)
     cnt = 0
-
-    for _, tan in c_list:
+    for tan in sorted(Counter(tangerine).values(),reverse=True):
         cnt += 1
         k -= tan
         if k <= 0:
             break
-        
-    
     return cnt
