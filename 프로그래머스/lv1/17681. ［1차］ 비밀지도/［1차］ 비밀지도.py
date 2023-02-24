19164d64fa396167aca1712a655e2b8555f1ffb9
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-02-24 22:52:40 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,15 +1,26 @@
+import re
 def solution(n, arr1, arr2):
     answer = []
     # 크기 N
     for i in range(n):
-        line = ''
-        bin1 = str(bin(arr1[i]))[2:].zfill(n)
-        bin2 = str(bin(arr2[i]))[2:].zfill(n)
-        for j in range(n):
-            if int(bin1[j])+int(bin2[j])==0:
-                line += ' '
-            else:
-                line += '#'
-        answer.append(line)
+        result = bin(arr1[i] | arr2[i])[2:].zfill(n)
+        result = result.replace('1','#').replace('0',' ')
+        answer.append(result)
     
     return answer
+
+# def solution(n, arr1, arr2):
+#     answer = []
+#     # 크기 N
+#     for i in range(n):
+#         line = ''
+#         bin1 = str(bin(arr1[i]))[2:].zfill(n)
+#         bin2 = str(bin(arr2[i]))[2:].zfill(n)
+#         for j in range(n):
+#             if int(bin1[j])+int(bin2[j])==0:
+#                 line += ' '
+#             else:
+#                 line += '#'
+#         answer.append(line)
+    
+#     return answer
