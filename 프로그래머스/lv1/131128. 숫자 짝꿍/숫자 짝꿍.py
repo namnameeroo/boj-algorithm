57487ef5c77643f85a137c9618d081cce23aabda
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-04-03 02:58:58 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -1,11 +1,27 @@
 from collections import Counter
 def solution(X, Y):
     answer = ''
+    add =''
     cx = Counter(X)
     cy = Counter(Y)
-    for i in set(cx.keys())&set(cy.keys()):        
-        answer += str(i)*min(cx[i], cy[i])
-    if len(answer)==0: return "-1"
-    answer = sorted(answer, reverse=True)
-    if answer[0]=='0': return '0'
-    return ''.join(answer)
+    for i in sorted(list(set(cx.keys())&set(cy.keys())), reverse=True):
+        i = str(i)
+        add=i*min(cx[i],cy[i])
+        if i=='0' and len(answer) == 0:
+            answer += str(int(add))
+        else:
+            answer += add
+    return answer if answer else '-1'
+
+# from collections import Counter
+# def solution(X, Y):
+#     answer = ''
+#     cx = Counter(X)
+#     cy = Counter(Y)
+#     for i in set(cx.keys())&set(cy.keys()):        
+#         answer += str(i)*min(cx[i], cy[i])
+        
+#     if len(answer)==0: return "-1"
+#     answer = sorted(answer, reverse=True)
+#     if answer[0]=='0': return '0'
+#     return ''.join(answer)
