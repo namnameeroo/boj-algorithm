0f7834cd48003c7e9f7df207b2de0b0e7da9b718
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-04-12 13:22:14 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,6 @@
+def solution(name, yearning, photo):
+    
+    dic = {name[idx]:yearning[idx] for idx in range(len(name))}
+    answer = [sum([dic[r] for r in row if r in name]) for row in photo]
+    
+    return answer
