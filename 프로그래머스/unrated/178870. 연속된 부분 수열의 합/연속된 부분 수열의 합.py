d3276528f03e48cbacb97aef88cbde0e44957bbd
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2023-04-13 15:59:09 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -3,27 +3,20 @@ def solution(sequence, k):
     sp = 0
     ep = 1
     tot = sequence[0]
-    sequence+=[0]
-    answer = []
-    chance = 1
     
-    while not(sp >= ep or ep == len(sequence)):
+    sequence += [0]
+    answer = [10**7]
+    while sp < ep < len(sequence):
         if tot < k :
             tot += sequence[ep]
             ep += 1
             
-        elif tot > k:
+        elif tot == k and answer[0] > ep-sp :
+            answer = [ep-sp, sp, ep-1]
+            
+        else:
             tot -= sequence[sp]
             sp += 1
             
-        elif tot == k:
-            # 길이, 시작인덱스, 마지막인덱스
-            arr = [ep-sp, sp, ep-1]
-            if (not answer) or answer[0] > arr[0]:
-                answer = arr
-            elif answer[0] == arr[0] :
-                answer = arr if answer[1] > arr[1] else answer
-            tot -= sequence[sp]
-            sp += 1
     
     return answer[1:]
