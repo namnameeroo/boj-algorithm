c717a5ac848c5487025722262f598eadcd5251de
Nami <69442847+namnameeroo@users.noreply.github.com>
2023-09-23 00:21:05 +0900

    [unrated] Title: 카운트 업, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,8 @@
+function solution(start_num, end_num) {
+    var answer = [];
+    let insertnum = start_num;
+    for (let i = insertnum;  i<=end_num; i++){
+        answer.push(i);
+    }
+    return answer;
+}
